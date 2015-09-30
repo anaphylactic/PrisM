e1082592b71d54b53810a3ab90d56cec4eeac24d
anaarmas <anaarmasromero@gmail.com>
2015-09-30 15:52:32 +0100

    cleaned class for producing a normalised version of an ontology like the one PrisM works with internally

--- test/thesisTests/NormaliserViaClauses.java
+++ test/thesisTests/NormaliserViaClauses.java
@@ -7,18 +7,13 @@ import org.semanticweb.HermiT.model.Atom;
 import org.semanticweb.HermiT.model.DLClause;
 import org.semanticweb.HermiT.model.DLOntology;
 import org.semanticweb.HermiT.structural.OWLClausification;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDataPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLDifferentIndividualsAxiom;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLNegativeDataPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLNegativeObjectPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -26,8 +21,6 @@ import org.semanticweb.owlapi.model.OWLSameIndividualAxiom;
 import org.semanticweb.owlapi.model.OWLSubPropertyChainOfAxiom;
 import org.semanticweb.owlapi.model.OWLTransitiveObjectPropertyAxiom;
 
-import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
-import uk.ac.ox.cs.JRDFox.JRDFStoreException;
 import uk.ac.ox.cs.pagoda.approx.RLPlusOntology;
 import uk.ac.ox.cs.pagoda.constraints.NullaryBottom;
 import uk.ac.ox.cs.pagoda.owl.OWLHelper;
@@ -37,13 +30,6 @@ import uk.ac.ox.cs.prism.RedundantAxiomRepairer;
 
 public class NormaliserViaClauses {
 
-	
-//	LinkedList<OWLTransitiveObjectPropertyAxiom> transitiveAxioms;
-////	LinkedList<DLClause> transitiveClauses;
-//	
-//	LinkedList<OWLSubPropertyChainOfAxiom> subPropChainAxioms; 
-////	LinkedList<DLClause> subPropChainClauses;
-	
 	
 	public static Set<OWLAxiom> getNormalisedAxioms(OWLOntology o){
 		
@@ -83,13 +69,13 @@ public class NormaliserViaClauses {
 				else if (axiom instanceof OWLNegativeObjectPropertyAssertionAxiom)
 					ret.add(axiom);
 				else if (axiom instanceof OWLNegativeDataPropertyAssertionAxiom) {
-					System.out.println("IGNORING: " + axiom.toString());
+//					System.out.println("IGNORING: " + axiom.toString());
 				}
 				else if (axiom instanceof OWLDataPropertyAssertionAxiom) {
-					System.out.println("IGNORING: " + axiom.toString());
+//					System.out.println("IGNORING: " + axiom.toString());
 				}
 				else if (axiom instanceof OWLDataPropertyRangeAxiom) {
-					System.out.println("IGNORING: " + axiom.toString());
+//					System.out.println("IGNORING: " + axiom.toString());
 				} 
 				else {
 					manager.addAxiom(filteredOntology, axiom);
@@ -106,76 +92,67 @@ public class NormaliserViaClauses {
 		//these axioms would be added to the ABox in the class Program from PAGOdA
 		OWLAxiom axiom;
 		for (Atom atom : dlOntology.getPositiveFacts()) {
-			System.out.println("DEBUG: this was sent to positiveFacts" + atom.toString());
-			if ((axiom = OWLHelper.getABoxAssertion(manager.getOWLDataFactory(), atom)) != null){
+			if ((axiom = OWLHelper.getABoxAssertion(manager.getOWLDataFactory(), atom)) != null)
 				ret.add(axiom);
-				System.out.println("DEBUG: but it was recovered: " + axiom.toString());
-			}
-			else
-				System.out.println("DEBUG: and it wasn't recovered!");
 		}
 				
 		return ret;
 	}
 	
-	public static void main(String[] args) throws OWLOntologyCreationException, JRDFStoreException {
-		
-		
-		String iri = "file://dummy#";
-		OWLDataFactory factory = new OWLDataFactoryImpl();
-		
-		OWLClass a = factory.getOWLClass(IRI.create(iri+"A"));
-		OWLClass b = factory.getOWLClass(IRI.create(iri+"B"));
-		OWLClass c = factory.getOWLClass(IRI.create(iri+"C"));
-		OWLClass d = factory.getOWLClass(IRI.create(iri+"D"));
-		OWLClass e = factory.getOWLClass(IRI.create(iri+"E"));
-		OWLClass f = factory.getOWLClass(IRI.create(iri+"F"));
-		OWLClass g = factory.getOWLClass(IRI.create(iri+"G"));
-		OWLClass h = factory.getOWLClass(IRI.create(iri+"H"));
-		OWLObjectProperty r = factory.getOWLObjectProperty(IRI.create(iri+"R"));
-		OWLObjectProperty s = factory.getOWLObjectProperty(IRI.create(iri+"S"));
-		OWLNamedIndividual o = factory.getOWLNamedIndividual(IRI.create(iri+"o"));
-		OWLNamedIndividual i = factory.getOWLNamedIndividual(IRI.create(iri+"i"));
-//		OWLAxiom ax1 = factory.getOWLSubClassOfAxiom(a, factory.getOWLObjectSomeValuesFrom(r, b));
-//		OWLAxiom ax2 = factory.getOWLSubClassOfAxiom(a, factory.getOWLObjectHasValue(r, o));
-//		OWLAxiom ax3 = factory.getOWLSubClassOfAxiom(factory.getOWLObjectIntersectionOf(b, c), d);
-//		OWLAxiom ax4 = factory.getOWLSubClassOfAxiom(factory.getOWLObjectSomeValuesFrom(r, c), e);
-//		OWLAxiom ax5 = factory.getOWLSubClassOfAxiom(d, factory.getOWLObjectUnionOf(f, factory.getOWLObjectSomeValuesFrom(r, b)));
-//		OWLAxiom ax6 = factory.getOWLSubClassOfAxiom(f, factory.getOWLObjectSomeValuesFrom(s, factory.getOWLThing()));
-//		OWLAxiom ax7 = factory.getOWLSubClassOfAxiom(factory.getOWLObjectSomeValuesFrom(s, factory.getOWLThing()), h);
-//		OWLAxiom ax8 = factory.getOWLSubClassOfAxiom(g, h);
-//		OWLAxiom ax9 = factory.getOWLClassAssertionAxiom(a, o);
-		OWLAxiom ax10 = factory.getOWLDifferentIndividualsAxiom(i,o);
-		OWLAxiom ax11 = factory.getOWLSameIndividualAxiom(i,o);
-		OWLAxiom ax12 = factory.getOWLNegativeObjectPropertyAssertionAxiom(r, o, i);
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		OWLOntology ont = manager.createOntology(IRI.create(iri.replace("#", ".owl")));
-//		manager.addAxiom(ont, ax1);
-//		manager.addAxiom(ont, ax2);
-//		manager.addAxiom(ont, ax3);
-//		manager.addAxiom(ont, ax4);
-//		manager.addAxiom(ont, ax5);
-//		manager.addAxiom(ont, ax6);
-//		manager.addAxiom(ont, ax7);
-//		manager.addAxiom(ont, ax8);
-//		manager.addAxiom(ont, ax9);
-		manager.addAxiom(ont, ax10);
-		manager.addAxiom(ont, ax11);
-		manager.addAxiom(ont, ax12);
-
-		
-		
-		Set<String> actual = new HashSet<String>();
-		for (OWLAxiom ax : getNormalisedAxioms(ont)){
-			System.out.println(ax.toString());
-//			actual.add(ax.toString());
-		}
-//		System.out.println();
-//		Set<String> control = new HashSet<String>();
-//		control.add("SubClassOf(<file://dummy#A> ObjectHasValue(<file://dummy#R> <file://dummy#o>))");
-//		control.add("SubClassOf(<file://dummy#A> ObjectSomeValuesFrom(<file://dummy#R> <file://dummy#B>))");
-		
-	}
+//	public static void main(String[] args) throws OWLOntologyCreationException, JRDFStoreException {
+//		
+//		String iri = "file://dummy#";
+//		OWLDataFactory factory = new OWLDataFactoryImpl();
+//		
+//		OWLClass a = factory.getOWLClass(IRI.create(iri+"A"));
+//		OWLClass b = factory.getOWLClass(IRI.create(iri+"B"));
+//		OWLClass c = factory.getOWLClass(IRI.create(iri+"C"));
+//		OWLClass d = factory.getOWLClass(IRI.create(iri+"D"));
+//		OWLClass e = factory.getOWLClass(IRI.create(iri+"E"));
+//		OWLClass f = factory.getOWLClass(IRI.create(iri+"F"));
+//		OWLClass g = factory.getOWLClass(IRI.create(iri+"G"));
+//		OWLClass h = factory.getOWLClass(IRI.create(iri+"H"));
+//		OWLObjectProperty r = factory.getOWLObjectProperty(IRI.create(iri+"R"));
+//		OWLObjectProperty s = factory.getOWLObjectProperty(IRI.create(iri+"S"));
+//		OWLNamedIndividual o = factory.getOWLNamedIndividual(IRI.create(iri+"o"));
+//		OWLNamedIndividual i = factory.getOWLNamedIndividual(IRI.create(iri+"i"));
+////		OWLAxiom ax1 = factory.getOWLSubClassOfAxiom(a, factory.getOWLObjectSomeValuesFrom(r, b));
+////		OWLAxiom ax2 = factory.getOWLSubClassOfAxiom(a, factory.getOWLObjectHasValue(r, o));
+////		OWLAxiom ax3 = factory.getOWLSubClassOfAxiom(factory.getOWLObjectIntersectionOf(b, c), d);
+////		OWLAxiom ax4 = factory.getOWLSubClassOfAxiom(factory.getOWLObjectSomeValuesFrom(r, c), e);
+////		OWLAxiom ax5 = factory.getOWLSubClassOfAxiom(d, factory.getOWLObjectUnionOf(f, factory.getOWLObjectSomeValuesFrom(r, b)));
+////		OWLAxiom ax6 = factory.getOWLSubClassOfAxiom(f, factory.getOWLObjectSomeValuesFrom(s, factory.getOWLThing()));
+////		OWLAxiom ax7 = factory.getOWLSubClassOfAxiom(factory.getOWLObjectSomeValuesFrom(s, factory.getOWLThing()), h);
+////		OWLAxiom ax8 = factory.getOWLSubClassOfAxiom(g, h);
+////		OWLAxiom ax9 = factory.getOWLClassAssertionAxiom(a, o);
+//		OWLAxiom ax10 = factory.getOWLDifferentIndividualsAxiom(i,o);
+//		OWLAxiom ax11 = factory.getOWLSameIndividualAxiom(i,o);
+//		OWLAxiom ax12 = factory.getOWLNegativeObjectPropertyAssertionAxiom(r, o, i);
+//		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+//		OWLOntology ont = manager.createOntology(IRI.create(iri.replace("#", ".owl")));
+////		manager.addAxiom(ont, ax1);
+////		manager.addAxiom(ont, ax2);
+////		manager.addAxiom(ont, ax3);
+////		manager.addAxiom(ont, ax4);
+////		manager.addAxiom(ont, ax5);
+////		manager.addAxiom(ont, ax6);
+////		manager.addAxiom(ont, ax7);
+////		manager.addAxiom(ont, ax8);
+////		manager.addAxiom(ont, ax9);
+//		manager.addAxiom(ont, ax10);
+//		manager.addAxiom(ont, ax11);
+//		manager.addAxiom(ont, ax12);
+//
+//		
+//		
+//		ModuleExtractionUpperProgram prog = new ModuleExtractionUpperProgram(new IndividualManager(InseparabilityRelation.CLASSIFICATION_INSEPARABILITY));
+//		prog.load(ont, new UnaryBottom());
+//		
+//		for (OWLAxiom ax : getNormalisedAxioms(ont)){
+//			System.out.println(ax.toString());
+//		}
+//		
+//	}
 	
 	
 }
